3c3f78d69fdc7f4a12ceb3d4dc0114d1bc41373a
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2011-08-07 16:22:57 +0100

    picker default to 10

--- Start.m
+++ Start.m
@@ -64,6 +64,7 @@
         CustomDataSource = [[CustomPickerDataSource_Num_Questions alloc] init];
         QuestionPickerView.delegate = CustomDataSource;
         QuestionPickerView.dataSource = CustomDataSource;
+        [QuestionPickerView selectRow:9 inComponent:1 animated:YES];  // sets the default on the PickerView to 10
     }
 
 	[self willAnimateRotationToInterfaceOrientation:self.interfaceOrientation duration:1];
