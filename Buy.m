4f3802fb42f1cbe9c01de1148f1a087330db7bdc
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2013-02-28 10:08:54 +0000

    restore

--- Buy.m
+++ Buy.m
@@ -12,7 +12,7 @@
 
 @implementation Buy
 
-@synthesize ProductFromIstore,ProductsToIstore,ProductsToIStoreInArray,SortedDisplayProducts,observer;
+@synthesize ProductFromIstore,ProductsToIstore,ProductsToIStoreInArray,SortedDisplayProducts,observer,Restore;
 
 int dontShowPriceList = 0;
 #pragma mark -
@@ -239,7 +239,7 @@ int dontShowPriceList = 0;
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
    
-    return [SortedDisplayProducts count];
+    return [SortedDisplayProducts count] + 1;
 	
 }
 
@@ -266,6 +266,25 @@ int dontShowPriceList = 0;
 	}
 	else{
 		
+        if (indexPath.row ==  [SortedDisplayProducts count]){
+            if(!Restore){
+                Restore = [UIButton buttonWithType:UIButtonTypeCustom];
+            }
+            Restore.frame = CGRectMake(600, 2, 105, 39);
+            Restore.tag = indexPath.row + 1;
+            UIImage *RestoreImage = [UIImage imageNamed:@"restore.png"];
+            [Restore setBackgroundImage:RestoreImage forState:UIControlStateNormal];
+            [Restore addTarget:self action:@selector(BuyQuestion:) forControlEvents:UIControlEventTouchUpInside];
+            [cell.contentView addSubview:Restore];
+            cell.selectionStyle = UITableViewCellSelectionStyleNone;
+            cell.textLabel.text = @"Restore Purchases";
+            cell.detailTextLabel.text = @"0.00";
+            
+        }
+        else{
+            
+            if ([SortedDisplayProducts count] > 0){
+
 		  
 	SKProduct *product = [SortedDisplayProducts objectAtIndex:indexPath.row];
 	
@@ -291,7 +310,11 @@ int dontShowPriceList = 0;
 	cell.textLabel.text = [product localizedTitle];
 	[numberFormatter release];
 	
-		}
+		
+            }
+        
+        }
+    }
     
     return cell;
 }
@@ -333,7 +356,11 @@ int dontShowPriceList = 0;
 					SKPayment *payment4 = [SKPayment paymentWithProductIdentifier:@"com.LearnersCloud.iEvaluatorForiPad.English.1040"];
 					[[SKPaymentQueue defaultQueue] addPayment:payment4];
 					break;
-
+                case 5:
+					;
+					[[SKPaymentQueue defaultQueue] addTransactionObserver:observer];
+                    [[SKPaymentQueue defaultQueue]restoreCompletedTransactions];
+					break;
 				
 			}
 			
@@ -359,7 +386,11 @@ int dontShowPriceList = 0;
 					[[SKPaymentQueue defaultQueue] addPayment:payment3];
 					
 					break;	
-					
+                case 4:
+					;
+					[[SKPaymentQueue defaultQueue] addTransactionObserver:observer];
+                    [[SKPaymentQueue defaultQueue]restoreCompletedTransactions];
+					break;
 				
 			}
 						
@@ -378,6 +409,11 @@ int dontShowPriceList = 0;
 					[[SKPaymentQueue defaultQueue] addPayment:payment2];
 
 					break;
+                case 3:
+					;
+					[[SKPaymentQueue defaultQueue] addTransactionObserver:observer];
+                    [[SKPaymentQueue defaultQueue]restoreCompletedTransactions];
+					break;
 			}
 			
 		case 4:
@@ -387,6 +423,11 @@ int dontShowPriceList = 0;
 					;
 					SKPayment *payment1 = [SKPayment paymentWithProductIdentifier:@"com.LearnersCloud.iEvaluatorForiPad.English.750To1040"];
 					[[SKPaymentQueue defaultQueue] addPayment:payment1];
+					break;
+                case 2:
+					;
+					[[SKPaymentQueue defaultQueue] addTransactionObserver:observer];
+                    [[SKPaymentQueue defaultQueue]restoreCompletedTransactions];
 					break;
 				
 			}
